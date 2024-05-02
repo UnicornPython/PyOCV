81fb89f82120dd1aea1421630fd467be58089eac
ancion <ancion.github.com>
2024-05-02 23:20:34 +0800

    fix-bug: reome unused line

--- handle_video/videoWrite.py
+++ handle_video/videoWrite.py
@@ -14,9 +14,6 @@ def main():
     # 开启摄像头抓取视频帧
     cap = cv2.VideoCapture(0)
 
-    if not cap.isOpened:
-        
-
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
